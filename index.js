81245c5cd57a4278f5805145e70f4ba4c026a54f
nvictorino <nvictorino@gmail.com>
2018-05-31 13:29:55 -0400

    test

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express()
 app.use('/public', express.static('public'))
 
 app.get('/*', function(req, res) {
-  res.sendFile(__dirname + '/index.html')
+  res.sendFile(__dirname + '/test.html')
 })
 
 app.listen(8080, function() {
